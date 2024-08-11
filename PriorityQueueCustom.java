be28c291d9d1a13a5c7cfec4d989fc532d635e52
Darian Salehi <dariansal61@gmail.com>
2024-08-10 22:07:34 -0400

    Create priority queue using custom heap as underlying structure.

---
+++ PriorityQueueCustom.java
@@ -0,0 +1,16 @@
+public class PriorityQueueCustom {
+    private Heap heap = new Heap();
+
+    public void enqueue(int item) {
+        heap.add(item);
+    }
+
+    public int dequeue() {
+        return heap.remove();
+    }
+
+    public boolean isEmpty() {
+        return heap.isEmpty();
+    }
+
+}
