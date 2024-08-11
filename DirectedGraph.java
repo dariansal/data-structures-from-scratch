fe6d3aee14a01fe319f2dc3ed484d02f991f25d2
Darian Salehi <dariansal61@gmail.com>
2024-08-10 22:00:56 -0400

    Implement directed graph using adjacency list.
    - Add methods for adding and removing nodes and edges
    - Implement depth-first and breadth-first traversal
    - Include toString method for graph representation

---
+++ DirectedGraph.java
@@ -0,0 +1,146 @@
+import java.util.*;
+
+public class DirectedGraph {
+    private class Node {
+        private String info;
+
+        public Node(String info) {
+            this.info = info;
+        }
+
+        public String toString() {
+            return info;
+        }
+    }
+
+    private Map<String, Node> nodes = new HashMap<>();
+    // instance variables of graph: every graph has many nodes
+    // we could make this a list, but it would take a while to only put unique
+    // values in map
+    // the label of each node in the graph is the key
+    // we can use this to quickly lookup strings to see if we have nodes containing
+    // them
+    private Map<Node, List<Node>> adjacencyList = new HashMap<>();
+    // The hash table is automatically implemented using a map for quick lookup
+    // Stil
+
+    public void addNode(String info) {
+        var node = new Node(info);
+        nodes.putIfAbsent(info, node);
+        // we add a node to our graph if it is not in the graph
+        adjacencyList.putIfAbsent(node, new ArrayList<>());
+    }
+
+    public void removeNode(String info) {
+        Node node = nodes.get(info);
+        if (node == null)
+            return;
+        // Don't yell at them with exception because if node was already deleted then
+        // it's ok
+        // we also have to remove all of the connections to this node
+        for (Node each : adjacencyList.keySet())
+            adjacencyList.get(each).remove(node);
+        // for each node's list of connections, we are removing the node we are deleting
+        // removes all of the edges that the other nodes have tothat node
+        adjacencyList.remove(node);
+        nodes.remove(node);
+    }
+
+    public void addEdge(String from, String to) {
+        var fromNode = nodes.get(from);
+        var toNode = nodes.get(to);
+
+        if (fromNode == null || toNode == null)
+            throw new IllegalArgumentException();
+
+        adjacencyList.get(fromNode).add(toNode);
+        // adjacencyList.get(toNode).add(fromNode); becomes undirected graph
+
+        // if you add connection/edge from "john" to "josh" which are already in graph
+        // Access list of nodes from AdjacencyList HashMap, add the node its connected
+        // to
+
+    }
+
+    public void removeEdge(String from, String to) {
+        var fromNode = nodes.get(from);
+        var toNode = nodes.get(to);
+
+        if (fromNode == null || toNode == null)
+            return;
+
+        adjacencyList.get(fromNode).remove(toNode);
+    }
+
+    public void depthFirstTraversal(String root) {
+        if (nodes.get(root) == null)
+            return;
+        Node node = nodes.get(root);
+        depthFirstTraversal(node);
+    }
+
+    private void depthFirstTraversal(Node root) {
+        Stack<Node> stack = new Stack<>();
+        Set<Node> visited = new HashSet<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            Node removed = stack.pop();
+
+            if (visited.contains(removed))
+                continue;
+
+            visited.add(removed);
+            System.out.println(removed);
+            for (Node neighbor : adjacencyList.get(removed)) {
+                if (!visited.contains(neighbor)) {
+                    stack.push(neighbor);
+                }
+            }
+        }
+    }
+
+    public void breadthFirstTraversal(String root) {
+        if (nodes.get(root) == null)
+            return;
+        Node node = nodes.get(root);
+        breadthFirstTraversal(node);
+    }
+
+    private void breadthFirstTraversal(Node root) {
+        Queue<Node> queue = new ArrayDeque<>();
+        Set<Node> visited = new HashSet<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            Node removed = queue.remove();
+
+            if (visited.contains(removed))
+                continue;
+
+            visited.add(removed);
+            System.out.println(removed);
+            for (Node neighbor : adjacencyList.get(removed)) {
+                if (!visited.contains(neighbor)) {
+                    queue.remove(neighbor);
+                }
+            }
+        }
+    }
+
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+
+        for (Node source : adjacencyList.keySet()) {
+            List<Node> targets = adjacencyList.get(source);
+            if (!targets.isEmpty())
+                result.append(source.toString()).append(" is connected to ").append(targets).append("\n");
+            // appending \n adds new line
+
+        }
+        // for every node of graph, if it is connected to something
+        // print out the list of nodes it's connected to
+
+        return result.toString();
+        // We have toString method for a node and graph objects
+    }
+
+}
---
+++ Heap.java
@@ -0,0 +1,99 @@
+import java.util.Arrays;
+
+public class Heap {
+    private static final int INITIAL_CAPACITY = 20;
+    // Common to write final instance variables of class LIKE_THIS
+
+    private int[] nodes = new int[INITIAL_CAPACITY];
+    int i = 0; // index of last element of heap
+
+    // involves bubbling up
+    public void add(int number) {
+        ensureCapacity();
+
+        nodes[i] = number;
+        int currentIndex = i;
+        // Need separate variable so that you can follow along the node that
+        // you bubble up without losing count of your last index in the array
+        while (i > 0 && nodes[currentIndex] > nodes[parentIndex(currentIndex)]) {
+            // while current node is bigger than it's parent, swap their values
+            int parentIndex = parentIndex(currentIndex); // (currentI - 1/2)
+            swap(currentIndex, parentIndex);
+            currentIndex = parentIndex; // new index is index of previous parent
+            // Now that we swapped nodes by bubbling our node up, we need our current index
+            // back on the node that we moved up
+        }
+        i++;
+
+    }
+
+    // involves bubbling down
+    public int remove() {
+        if (i == 0)
+            throw new IllegalArgumentException("Empty HEAP");
+
+        int removedElement = nodes[0];
+        nodes[0] = nodes[i];
+        nodes[i] = 0;
+        int currentIndex = 0;
+        // start at root and go down
+
+        while (nodes[currentIndex] < maxChildValue(currentIndex)) {
+            // while the node is less than its largest child, swap and bubble node down
+            int largerChildIndex = largerChildIndex(currentIndex); // (2*currentI +1) or + 2
+            swap(currentIndex, largerChildIndex);
+            currentIndex = largerChildIndex;
+            // Current index changes because we moved our node down
+        }
+        i--;
+        return removedElement;
+        // we deleted a node
+
+    }
+
+    private void ensureCapacity() {
+        if (i == nodes.length) {
+            nodes = Arrays.copyOf(nodes, 2 * nodes.length);
+            // Arrays.copyOf(arrayTobeCopied, newLengthofArray)
+        }
+    }
+
+    public boolean isEmpty() {
+        return i == 0;
+    }
+
+    private void swap(int i, int j) {
+        int temp = nodes[i];
+        nodes[i] = nodes[j];
+        nodes[j] = temp;
+    }
+
+    private int parentIndex(int index) {
+        return (index - 1) / 2;
+        // Gives index of parent for your current node
+    }
+
+    private int leftChildIndex(int index) {
+        return 2 * index + 1;
+    }
+
+    private int rightChildIndex(int index) {
+        return 2 * index + 2;
+    }
+
+    private int maxChildValue(int index) {
+        return Math.max(nodes[leftChildIndex(index)], nodes[rightChildIndex(index)]);
+    }
+
+    private int largerChildIndex(int index) {
+        return (nodes[leftChildIndex(index)] > nodes[rightChildIndex(index)]) ? leftChildIndex(index)
+                : rightChildIndex(index);
+        // if leftChild is greater, return index of leftchild, else, return rightchild
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(nodes);
+    }
+
+}
